bab5487150d9a8076a616cf93138ea361b81cc29
rahim <rahimdev786@gmail.com>
2022-04-28 14:07:18 +0530

    Comit

--- UIImagesPickers/ViewController.swift
+++ UIImagesPickers/ViewController.swift
@@ -8,12 +8,45 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var imagePicker:UIImageView!{
+        didSet{
+            self.imagePicker.contentMode = .scaleAspectFill
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+      
     }
-
-
+    @IBAction func getPicture(){
+        self.setUpImagePicker()
+    }
 }
 
+extension ViewController{
+    func setUpImagePicker(){
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.sourceType = .photoLibrary
+        picker.allowsEditing = true
+        present(picker, animated: true)
+        
+    }
+}
+
+extension ViewController:UIImagePickerControllerDelegate,UINavigationControllerDelegate{
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        if let image = info[.originalImage] as? UIImage{
+            imagePicker.image = image
+        }
+        picker.dismiss(animated: true)
+        
+    }
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true)
+    }
+}
